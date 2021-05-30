96299c1df29e7f46b2862ef9840e108ca3a3c56d
shankol <85067548+shankol@users.noreply.github.com>
2021-05-30 19:59:31 +0500

    first commit

---
+++ lab 1task1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the number= "))
+b=3
+c=a+b
+print("Adding 3=",c)
+d=2*c
+print("After Multiply by 2= ",d)
+e=d-4
+print("After subtraction= " ,e)
+f=e+3
+print("After adding 3 again= ",f)
+            
